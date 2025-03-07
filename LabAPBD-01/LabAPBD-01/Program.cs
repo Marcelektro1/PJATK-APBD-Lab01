a6d7e91a34a1df1bfbc43e5cef337e9bf97bde4e
s30500 <s30500@pjwstk.edu.pl>
2025-03-07 13:19:00 +0100

    Change 2

--- LabAPBD-01/LabAPBD-01/Program.cs
+++ LabAPBD-01/LabAPBD-01/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-String user = "Username";
+using LabAPBD_01;
+
+User user = new User("Username", 19);
 
 Console.WriteLine($"Hello, {user}!");
+Console.WriteLine($"You are {user.age} years old!");
---
+++ LabAPBD-01/LabAPBD-01/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LabAPBD_01;
+
+public class User
+{
+
+    public string username { get; private set; }
+    public int age { get; private set; }
+
+    public User(string username, int age)
+    {
+        this.username = username;
+        this.age = age;
+    }
+    
+}
